1507e08e49581b99974fa3840651f58c98a14ecc
pratibhachaurasia <63347820+pratibhachaurasia@users.noreply.github.com>
2020-04-11 11:13:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		   System.out.println("Hellooo world!");
+		   System.out.println("Hellooo world! - made some changes to check rebuils trigger");
 	}
 
 }
